7193e3848c7e7482898b223a7e2ff6cf2599ee97
Iker Z. <ikerunai.zambrano@gmail.com>
2025-11-06 18:15:12 +0100

    feat(mappings): added git diff preview side to side mapping

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -9,5 +9,8 @@ map("i", "jk", "<ESC>")
 map({ "n", "x", "v" }, "<leader>ca", function()
   require("tiny-code-action").code_action()
 end, { noremap = true, silent = true, desc = "Show Code Actions" })
+map({ "n", "x", "v" }, "<leader>gd", function()
+  require("gitsigns").diffthis()
+end, { noremap = true, silent = true, desc = "Show Buffer Diff" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
